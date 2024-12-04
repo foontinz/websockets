c5d8fca230bbe2cefbcd139a3a66291f8a96d92f
Dmytro Yurinov <foontinz@protonmail.com>
2024-12-04 17:12:10 +0100

    adds configureConnection, refactors server.go.

--- server/server.go
+++ server/server.go
@@ -19,7 +19,7 @@ type ProxyServer struct {
 }
 
 func NewProxyServer() *ProxyServer {
-	var s *ProxyServer
+	s := &ProxyServer{}
 	s.upgrader = websocket.Upgrader{
 		HandshakeTimeout: time.Second * 30,
 		ReadBufferSize:   1024,
@@ -39,20 +39,12 @@ func (ps *ProxyServer) tryUpgradeConn(w http.ResponseWriter, r *http.Request) (*
 
 	conn, err := ps.upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("SERVER: Error in upgrading:", err)
+		log.Println("SERVER: Errored in upgrading:", err)
 		return nil, err
 	}
 	return conn, nil
 }
-
-func (ps *ProxyServer) HandleConnections(w http.ResponseWriter, r *http.Request) {
-	conn, err := ps.tryUpgradeConn(w, r)
-
-	defer func() {
-		log.Println("SERVER: Closing connection")
-		conn.Close()
-	}()
-
+func (ps *ProxyServer) configureConnection(conn *websocket.Conn) *websocket.Conn {
 	// Set timeouts for read/write to avoid lingering connections (added this block)
 	conn.SetReadLimit(512)
 	conn.SetReadDeadline(time.Now().Add(60 * time.Second))
@@ -60,12 +52,30 @@ func (ps *ProxyServer) HandleConnections(w http.ResponseWriter, r *http.Request)
 		conn.SetReadDeadline(time.Now().Add(60 * time.Second))
 		return nil
 	})
+	return conn
+}
+
+func (ps *ProxyServer) HandleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := ps.tryUpgradeConn(w, r)
+	if err != nil {
+		log.Println("SERVER: Failed to upgrade connection to websockets.")
+		return
+	}
+	defer func() {
+		log.Println("SERVER: Closing connection")
+		conn.Close()
+	}()
+	conn = ps.configureConnection(conn)
 
 	for {
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			if !websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Println("SERVER: Unexpected close error:", err)
+				break
+			}
+			if errors.Is(err, io.ErrUnexpectedEOF) {
+				log.Println("SERVER: Unexpected eof error:", err)
 				break
 			}
 			log.Println("SERVER: Error during reading message:", err)
@@ -80,14 +90,15 @@ func (ps *ProxyServer) HandleConnections(w http.ResponseWriter, r *http.Request)
 		err = conn.WriteMessage(websocket.TextMessage, message)
 		if err != nil {
 			log.Println("SERVER: Error during echoing message:", err)
-			return // Modified to close the loop on write error
+			return
 		}
 		log.Printf("SERVER: Sent to client: %s\n", message)
 	}
 }
 
 func StartServer(addr string) {
-	http.HandleFunc("/ws", HandleConnections)
+	server := NewProxyServer()
+	http.HandleFunc("/ws", server.HandleConnections)
 	log.Println("SERVER: WebSocket server starting on :8080")
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("SERVER: Server failed: %v", err)
