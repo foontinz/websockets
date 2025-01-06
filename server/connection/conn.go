e8228ecec7fddc0f6abce0f50808f0d840fab68e
Dmytro Yurinov <foontinz@protonmail.com>
2025-01-06 22:27:29 +0100

    feat(server/connection): adds ctxCancel to connection's close handler

--- server/connection/conn.go
+++ server/connection/conn.go
@@ -1,15 +1,20 @@
 package connection
 
 import (
+	"context"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-func ConfigureConnection(conn *websocket.Conn) *websocket.Conn {
+func ConfigureConnection(conn *websocket.Conn, ctxCancel context.CancelFunc) *websocket.Conn {
 	// Set timeouts for read/write to avoid lingering connections (added this block)
 	conn.SetReadLimit(512)
 	conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	conn.SetCloseHandler(func(code int, text string) error {
+		ctxCancel()
+		return conn.CloseHandler()(code, text)
+	})
 	conn.SetPongHandler(func(string) error {
 		conn.SetReadDeadline(time.Now().Add(60 * time.Second))
 		return nil
