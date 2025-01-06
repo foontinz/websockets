c116bbef748f878ca56fe2a44b908d4f6bf0f136
Dmytro Yurinov <foontinz@protonmail.com>
2025-01-06 22:26:35 +0100

    feat(sink): shrinks sink interface

--- sink/sink.go
+++ sink/sink.go
@@ -6,6 +6,6 @@ import (
 
 type Sink interface {
 	Write(ctx context.Context, channels string, data interface{}) error
-	Subscribe(ctx context.Context, channels ...string) (<-chan string, <-chan struct{})
+	// Subscribe(ctx context.Context, channels ...string) (<-chan string, <-chan struct{}) TODO: might be extended in the future
 	Close(ctx context.Context) error
 }
